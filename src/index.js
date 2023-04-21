aba82d137d87ada2040277b623b367be155b8795
Krishna Kushwaha <120515502+krishna9788@users.noreply.github.com>
2023-04-21 17:35:53 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,104 +1,51 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 8080
-
+const port = 8080;
+const { connection } = require("./connector");
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-const { connections } = require('mongoose');
-const { connection } = require('./connector');
+const { connections } = require("mongoose");
 
-const isNullOrUndefined = val => val === null || val === undefined;
+app.get("/findColleges", async (req, res) => {
+  let data;
+  const name = req.query.name === undefined ? "" : req.query.name;
+  const state = req.query.state === undefined ? "" : req.query.state;
+  const city = req.query.city === undefined ? "" : req.query.city;
+  const minPackage = Number(
+    req.query.minPackage === undefined
+      ? Number(0)
+      : isNaN(req.query.minPackage)
+      ? Number(0)
+      : req.query.minPackage
+  );
+  const maxFees =
+    req.query.maxFees === undefined
+      ? Number(1000)
+      : isNaN(req.query.maxFees)
+      ? Number(1000)
+      : Number(req.query.maxFees);
 
-app.get("/findColleges", async (req, res)=>{
-    const name = req.query.name;
-    const state = req.query.state;
-    const city = req.query.city;
-    const minPackage = req.query.minPackage;
-    const maxFees = req.query.maxFees;
-    const course = req.query.course;
-    const exam = req.query.exam;
-    let result = [];
-    let combineQuery = [];
+  const course = req.query.course === undefined ? "" : req.query.course;
+  const exam = req.query.exam === undefined ? "" : req.query.exam;
+  console.log(minPackage === 0);
+  if (Object.keys(req.query).length !== 0) {
+    data = await connection.find({
+      name: { $regex: `${name}`, $options: "i" },
+      state: { $regex: `${state}`, $options: "i" },
+      city: { $regex: `${city}`, $options: "i" },
+      minPackage: { $gte: `${minPackage}` },
+      maxFees: { $lte: `${maxFees}` },
+      course: { $regex: `${course}`, $options: "i" },
+      exam: { $regex: `${exam}`, $options: "i" },
+    });
+  } else {
+    data = await connection.find();
+  }
+  res.send(data);
+});
 
-    for(let key in req.query){
-        if(key === 'name'){
-            combineQuery.push({name: {$regex: req.query[key], $options:"i"}});
-        }
-        else if(key === 'state'){
-            combineQuery.push({state: {$regex: req.query[key], $options:"i"}});
-        }else if(key === 'city'){
-            combineQuery.push({city: {$regex: req.query[key], $options:"i"}});
-        }else if(key === 'minPackage'){
-            combineQuery.push({minPackage:{$gte : parseFloat(req.query[key])}});
-        }else if(key === 'maxFees'){
-            combineQuery.push({maxFees:{$lte : parseFloat(req.query[key])}});
-        }else if(key === 'course'){
-            combineQuery.push({course: {$regex: req.query[key], $options:"i"}});
-        }else if(key === 'exam'){
-            combineQuery.push({exam: {$regex: req.query[key], $options:"i"}});
-        }
-    }
-
-    if(Object.keys(req.query).length > 1){
-        const listOfCombinCollege = await connection.find({$and: combineQuery})
-        listOfCombinCollege.forEach(element =>{
-            result.push(element);
-        })
-        res.send(result);
-    }
-    else if(isNullOrUndefined(name) && isNullOrUndefined(state) && isNullOrUndefined(city) && isNullOrUndefined(minPackage) && isNullOrUndefined(maxFees) && isNullOrUndefined(course) && isNullOrUndefined(exam)){
-        res.send(await connection.find());
-    }else{
-        if(!isNullOrUndefined(name)){
-            const listOfName = await connection.find({name:{$regex:name,$options:"$i"}});
-            listOfName.forEach(element => {
-                result.push(element);
-            });    
-        }
-        else if(!isNullOrUndefined(state)){
-            const listOfstate = await connection.find({state:{$regex:state,$options:"$i"}});
-            listOfstate.forEach(element => {
-                result.push(element);
-            });    
-        }
-        else if(!isNullOrUndefined(city)){
-            const listOfcity = await connection.find({city:{$regex:city,$options:"$i"}});
-            listOfcity.forEach(element => {
-                result.push(element);
-            });    
-        }
-        else if(!isNullOrUndefined(minPackage) && minPackage > 0){
-            const listOfminPackage = await connection.find({minPackage:{$gte : parseFloat(minPackage)}});
-            listOfminPackage.forEach(element => {
-                result.push(element);
-            });    
-        }
-        else if(!isNullOrUndefined(maxFees) && maxFees > 0){
-            const listOfmaxFees = await connection.find({maxFees:{$lte : parseFloat(maxFees)}});
-            listOfmaxFees.forEach(element => {
-                result.push(element);
-            });    
-        }
-        else if(!isNullOrUndefined(course)){
-            const listOfcourse = await connection.find({course:{$regex:course,$options:"$i"}});
-            listOfcourse.forEach(element => {
-                result.push(element);
-            });    
-        }
-        else if(!isNullOrUndefined(exam)){
-            const listOfexams = await connection.find({exam:{$all:exam}});
-            listOfexams.forEach(element => {
-                result.push(element);
-            });    
-        }
-        res.send(result);
-    }
-
-})
-
-
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
